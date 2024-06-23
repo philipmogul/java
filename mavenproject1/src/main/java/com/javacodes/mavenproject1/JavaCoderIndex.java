4119d18c7227629f83815a5df2a17a638fbc32fe
philipmogul <philipp1mogul@gmail.com>
2024-06-24 01:01:53 +0300

    Welcome to Java Coder Verbose

---
+++ mavenproject1/src/main/java/com/javacodes/mavenproject1/JavaCoderIndex.java
@@ -0,0 +1,10 @@
+package com.javacodes.mavenproject1;
+
+public class JavaCoderIndex {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Verbose Java!");
+	}
+
+}
---
+++ mavenproject1/src/main/java/com/javacodes/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.javacodes.mavenproject1;
+
+/**
+ *
+ * @author ekabiru
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Java Programming is Cool!");
+    }
+}
